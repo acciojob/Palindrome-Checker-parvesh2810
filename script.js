dd7bb772c9d2b8b8e75f1c4963b04046f03f9fa7
Parvesh Jain <parvesh2810@gmail.com>
2023-01-17 13:26:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 function palindrome(str){
 // find the length of a string
-    const len = string.length;
+    const len = str.length;
 
     // loop through half of the string
     for (let i = 0; i < len / 2; i++) {
 
         // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
+        if (str[i] !== str[len - 1 - i]) {
             return false;
         }
     }
